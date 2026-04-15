0a12141debc3a9142c7cc36793b9d351b9bce438
john <mkushmonov@gmail.com>
2026-04-15 20:29:42 +0900

    feat: Functions in Python

---
+++ funcitons.py
@@ -0,0 +1,30 @@
+'''
+FUNTIONS
+(1) define and call
+(2) parameter and argiment
+(3) keyword & default arguments
+(4) scope
+'''
+
+print("=========defone And call==========")
+# build in funciton > print() type()
+# function - reusable block of code!
+# Instead of block {} in java we use indentation
+
+
+# DEFINE
+def greet(a):
+    print(f"Welcome back, {a}")
+
+
+def greeting(b):
+    print("greeting is executed")
+    return f"Hello {b}"
+
+
+# CALL
+result1 = greet("John")
+print("result1:", result1)  # void
+
+result2 = greeting("Justin")
+print("result2:", result2)
