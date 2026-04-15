8076d9add52aa218a95f15859a9a57e82295e2eb
john <mkushmonov@gmail.com>
2026-04-15 20:12:47 +0900

    feat: Learn Boolean in Python

--- primitive.py
+++ primitive.py
@@ -35,3 +35,19 @@ print(f"The type of course: {result6}")
 print(result)
 
 print("=========boolean=========")
+# fucnitons > type() input() bool() int() str()
+y = input("Enter a value: ")
+print("Y value is:", y)
+
+result7 = y.isnumeric()
+print(f"The input value is numeric: {result7}")
+
+# TRUTHY & FALSY values
+# TRUTHY > True 10, -100, "srtring"
+# FALSY > False: False 0 "" None
+
+test_falsy = "" or None or False or 0 or 100
+print("The FALSY:", bool(test_falsy))
+
+test_truthy = "JOHN"
+print("The FALSY:", bool(test_truthy))
