a82543e0a9652994534a76999e42447729e209f2
john <mkushmonov@gmail.com>
2026-04-17 04:59:51 +0900

    feat: Objects in Python

---
+++ objects.py
@@ -0,0 +1,17 @@
+import array  # package
+import math  # package
+from math import ceil, asin  # mehtods
+
+print("WHAT IS OBJECT")
+
+print(type("Hello"))
+print(type(98))
+print(type(True))
+print(type(array))
+print(type(math))
+
+result = math.ceil(97.7)
+print(result)
+
+result1 = ceil(98.7)
+print(result1)
