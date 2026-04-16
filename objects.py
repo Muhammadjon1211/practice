1ac36973a6e4d455320948c0528035da7c22e209
john <mkushmonov@gmail.com>
2026-04-17 05:31:27 +0900

    feat: Error handling system in Python

--- objects.py
+++ objects.py
@@ -15,3 +15,21 @@ print(result)
 
 result1 = ceil(98.7)
 print(result1)
+
+
+print("=====Error Handling syste ==========")
+car_dict = dict(name="Toyota", year=2026, ev=True)
+
+try:
+    print("Passed here")
+    a = car_dict.speed
+    result = car_dict['year']
+    print("Result:", result)
+except KeyError as err:
+    print("No origin state property found:", err)
+except AttributeError as err:
+    print("No speed attribute found:", err)
+else:
+    print("Executed withour errors")
+finally:
+    print("Final clothing logic")
