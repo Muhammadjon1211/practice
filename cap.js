05c123eecd780c97df6443f9af5ee82467c79d57
john <mkushmonov@gmail.com>
2026-05-01 17:22:56 +0900

    fix: advantages of async/await function

--- cap.js
+++ cap.js
@@ -1,31 +1,20 @@
+
 async function division(a, b) {
-    return new Promise((resolve,reject) => {
         if (b===0) {
-            reject("Not divided by zero.");
+            throw new Error("Nod divided by ZERO.")
         } else {
-            setTimeout(function() {
-                resolve(a&b)
-            }, 2000)
+            return a%b
         }
-    })
+    }
+
+async function run() {
+    let result = await division(10,3);
+    console.log(result)
+
+    result = await division(10,4);
+    console.log(result)
+
+    result = await division(10,5);
+    console.log(result)
 }
-
-
-division(10,3).then(data => {
-    console.log("RESULT:", data);
-
-    division(10,4).then(data => {
-        console.log("RESULT:", data);
-
-        division(10,4).then(data => {
-            console.log("RESULT:", data);
-        }).catch(err=>{
-            console.log("Error division:", err)
-        });
-    }).catch(err=>{
-        console.log("Error division:", err)
-    });
-
-}).catch(err=>{
-    console.log("Error division:", err)
-});
+run()
